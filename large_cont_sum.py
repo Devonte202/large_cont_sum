28d9037632992029d80c9f6a2ee994d0f9ff801f
EC2 Default User <ec2-user@ip-172-31-16-222.us-east-2.compute.internal>
2020-02-05 14:56:06 +0000

    solved + edge cases taken into account

--- large_cont_sum.py
+++ large_cont_sum.py
@@ -1,2 +1,47 @@
-def large_cont_sum():
-    pass
+"""
+Problem: Given an array of integers find the largest continuous sum
+input: list (positive & negative integers)
+output: number(integers)
+
+Examples: 
+large_cont_sum([1,2,-1,3,4,10,10,-10,-1]) == 29
+large_cont_sum([1,2,-1,3,4,-1]) == 9
+large_cont_sum([-1,1]) == 1
+large_cont_sum([0,0,0,-1,1]) == 1
+large_cont_sum([-1,-2,-1,-3,4,10,10,-10,-1]) == 17
+large_cont_sum([-1,1,-1,-2,-1,-3,4,10,10,-10,-1]) == 17
+large_cont_sum([]) == None
+
+Data Structure: Dictionary 
+Algorithm: 
+0: Create a biggest_num_now variable initialized with 0
+1: Create a num_sum variable initialized with 0
+2: Loop through list
+    I: add each number to the num_sum variable
+    II: if num_sum is greater than biggest_num_now, reassign biggest_num_now to num_sum
+3: If the list only has two numbers in it with a sum of 0, biggest_num_now = the largest number in the list
+4: Return the biggest_num_now variable
+"""
+
+def large_cont_sum(num_list):
+    num_list = [num for num in num_list if num != 0]
+    biggest_num_now = 0
+    num_sum = 0
+    if len(num_list) == 0:
+        return None
+    if len(num_list) <= 2:
+        return max(num_list)
+    for num in num_list:
+        num_sum += num 
+        if num_sum > biggest_num_now:
+            biggest_num_now = num_sum
+    return biggest_num_now
+
+print(large_cont_sum([1,2,-1,3,4,10,10,-10,-1]) == 29)
+print(large_cont_sum([1,2,-1,3,4,-1]) == 9)
+print(large_cont_sum([-1,1]) == 1)
+print(large_cont_sum([0,0,0,-1,1]) == 1)
+print(large_cont_sum([-1,-2,-1,-3,4,10,10,-10,-1]) == 17)
+print(large_cont_sum([-1,1,-1,-2,-1,-3,4,10,10,-10,-1]) == 17)
+print(large_cont_sum([2]) == 2)
+print(large_cont_sum([]) == None)
